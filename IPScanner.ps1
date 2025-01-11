40d262fd77f6f3da56eea69a48b76f7600a4b94d
illsk1lls <gene.rovinsky@gmail.com>
2025-01-10 20:29:35 -0500

    Corrected comment

--- IPScanner.ps1
+++ IPScanner.ps1
@@ -1,4 +1,4 @@
-# Generate Admin request. Admin required to clear ARP cache for fresh network list - this is the only task it is required for, line #66
+# Generate Admin request. Admin required to clear ARP cache for fresh network list - this is the only task it is required for, line #65
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
 	Start-Process Powershell "-nop -ep bypass -f `"$PSCommandPath`"" -Verb RunAs
 	exit
