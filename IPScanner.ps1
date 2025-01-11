cae503355d043cd5ebdbf267c8e5635ad4e33ed1
illsk1lls <gene.rovinsky@gmail.com>
2025-01-11 11:54:22 -0500

    Cleanup - Added 5 second "Listening" timer
    Cleanup formatting
    
    Switched from - to : for mac display
    
    Added 5 second wait after scanning the subnet ( mass ping) to give devices a more time to respond.

--- IPScanner.ps1
+++ IPScanner.ps1
@@ -19,17 +19,17 @@ function Get-HostInfo {
 	$global:hostName = hostName
 
 	# Check Internet Connection and Get External IP
-	$ProgressPreference='SilentlyContinue'
+	$ProgressPreference = 'SilentlyContinue'
 	$hotspotRedirectionTest = irm "http://www.msftncsi.com/ncsi.txt"
 	$global:externalIP = if ($hotspotRedirectionTest -eq "Microsoft NCSI") { 
 		irm "http://ifconfig.me/ip"
 	} else { 
 		"No Internet or Redirection"
 	}
-	$ProgressPreference='Continue'
+	$ProgressPreference = 'Continue'
 	
 	# Find Gateway
-	$global:gateway = (Get-NetRoute -DestinationPrefix 0.0.0.0/0 | Select-Object -First 1).NextHop
+	$gateway = (Get-NetRoute -DestinationPrefix 0.0.0.0/0 | Select-Object -First 1).NextHop
 	$gatewayParts = $gateway -split '\.'
 	$global:gatewayPrefix = "$($gatewayParts[0]).$($gatewayParts[1]).$($gatewayParts[2])."
 
@@ -40,14 +40,14 @@ function Get-HostInfo {
 	$global:adapter = (Get-NetIPAddress -InterfaceAlias "*Ethernet*","*Wi-Fi*" -AddressFamily IPv4 | Where-Object { $_.IPAddress -like "$gatewayPrefix*" }).InterfaceAlias
 
 	# My Mac
-	$global:myMac = (Get-NetAdapter -Name $adapter).MacAddress
+	$global:myMac = (Get-NetAdapter -Name $adapter).MacAddress.Replace('-',':')
 
 	# Convert subnet prefix to readable number
 	$prefixLength = (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -ne 'Loopback Pseudo-Interface 1'} | Select-Object -First 1).PrefixLength
 	$subnetMask = ([System.Net.IPAddress]::Parse(($([Math]::Pow(2, $prefixLength)) - 1) * [Math]::Pow(2, 32 - $prefixLength))).GetAddressBytes() -join "."
 
 	# Domain
-	$global:domain = (Get-WmiObject -Class Win32_ComputerSystem).Domain
+	$domain = (Get-WmiObject -Class Win32_ComputerSystem).Domain
 
 	# Output results
 	$global:hostOutput = [PSCustomObject]@{
@@ -63,10 +63,10 @@ function Get-HostInfo {
 
 function Get-MacVendor($mac) {
 	# Get Vendor via Mac (thanks to u/mprz)
-	return (irm "https://www.macvendorlookup.com/api/v2/$($mac.Replace('-','').Substring(0,6))" -Method Get)
+	return (irm "https://www.macvendorlookup.com/api/v2/$($mac.Replace(':','').Substring(0,6))" -Method Get)
 }
 
-function Scan-Subnets {
+function Scan-Subnet {
 	# Clear ARP cache - Requires Admin
 	Remove-NetNeighbor -InterfaceAlias "$adapter" -AsJob -Confirm:$false | Out-Null
 
@@ -75,9 +75,20 @@ function Scan-Subnets {
 		Test-Connection $gatewayPrefix$i -Count 1 -AsJob | Out-Null
 		Write-Progress -Activity "Sending Packets" -Status "Progress..." -PercentComplete ($i * (100 / 254))
 	}
-	Write-Progress -Activity "Sending Packets" -Status "Complete!" -PercentComplete 100
+	Write-Progress -Activity "Sending Packets" -Status "Done" -PercentComplete 100
 	Start-Sleep -Seconds 1
 	Write-Progress -Activity "Sending Packets" -Completed
+}
+
+function waitForResponses {
+	# Wait with progress
+	for ($i = 1; $i -le 100; $i++) {
+		Write-Progress -Activity "Listening" -Status "Waiting for responses..." -PercentComplete ($i)
+		Start-Sleep -Milliseconds 50
+	}
+	Write-Progress -Activity "Listening" -Status "Done" -PercentComplete 100
+	Start-Sleep -Seconds 1
+	Write-Progress -Activity "Listening" -Completed	
 }
 
 function List-Machines {
@@ -92,13 +103,13 @@ function List-Machines {
 	$myLastOctet = [int]($internalIP -split '\.')[-1]
 	
 	# Get My Vendor via Mac lookup
-	$tryMyVendor=(Get-MacVendor "$myMac").Company
+	$tryMyVendor = (Get-MacVendor "$myMac").Company
 	$myVendor = if($tryMyVendor){$tryMyVendor.substring(0, [System.Math]::Min(25, $tryMyVendor.Length))} else {'Unknown'}
 
 	# Cycle through ARP table
 	foreach ($line in $arpOutput) {
 		$ip = $line.IPAddress
-		$mac = $line.LinkLayerAddress
+		$mac = $line.LinkLayerAddress.Replace('-',':')
 		$name = try {
 			([System.Net.Dns]::GetHostEntry($ip)).HostName
 		} catch {
@@ -131,12 +142,13 @@ do {
 	Clear-Host
 	Write-Host -NoNewLine 'Getting Ready...'
 	Get-HostInfo
-	Scan-Subnets
+	Scan-Subnet
+	waitForResponses
 	Write-Host 'Done';Write-Host
 	$hostOutput | Out-String -Stream | Where-Object { $_.Trim().Length -gt 0 } | Write-Host
-	$ProgressPreference='SilentlyContinue'
+	$ProgressPreference = 'SilentlyContinue'
 	List-Machines
-	$ProgressPreference='Continue'
+	$ProgressPreference = 'Continue'
 	Write-Host;Write-Host -NoNewLine 'Press any key to refresh, (X) to Exit'
 	$Host.UI.RawUI.Flushinputbuffer()
 } until ($Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown').VirtualKeyCode -eq 88)
